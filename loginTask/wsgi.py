45679c074ed2c72ca1f71e92498e45642ca83bf6
Vivian <healthecvivian@gmail.com>
2024-11-19 16:28:58 -0300

    Modificaciones Arhivo Wsgi.py y Vercel.json

--- loginTask/wsgi.py
+++ loginTask/wsgi.py
@@ -20,8 +20,8 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_app
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_heroku_task.settings')
 
-app = get_wsgi_app()
+app = get_wsgi_application()
